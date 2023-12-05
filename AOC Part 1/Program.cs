9ee27165aa8553bc8d7549d12407903aa29c3abe
23jrobinson <23jrobinson@JCC-D2022-04>
2023-12-05 10:15:22 +0000

    Add project files.

---
+++ AOC Part 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AOC_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader calibrationValues = new StreamReader("U:/AOC1-1.txt");
+
+            string[] calVal;
+
+            calVal = (calibrationValues.ReadToEnd()).Split('\n');
+            int x = 0;
+            bool y;
+            for (int i = 0; i <= 1000 ; i++)
+            {
+                
+            }
+        }
+    }
+}
